63f950b10cead2bd48616cc260fab3aef30e2a03
Jesus Garcia <35648860+jesusgarcia149@users.noreply.github.com>
2021-11-01 20:50:44 -0400

    Update lubuntu-to-media-center.sh

--- lubuntu-to-media-center.sh
+++ lubuntu-to-media-center.sh
@@ -37,9 +37,6 @@ sudo add-apt-repository ppa:libretro/stable -y
 sudo apt-get update
 sudo apt install retroarch -y
 
-# Intercambiar archivos de Android al PC
-sudo apt install kdeconnect -y
-
 
 
 
@@ -52,48 +49,6 @@ cd .config
 cd lxsession
 cd Lubuntu
 echo 'kodi' > autostart
-
-
-
-
-# Creamos los atajos con el teclado
-
-# Cambiar al directorio home
-cd
-
-#mkdir comands
-#cd comands
-
-#touch kodi.sh
-#echo '!#/bin/bash; pkill retroarch; pkill pcmanfm; kodi' > kodi.sh
-
-#touch retroarch.sh
-#echo '!#/bin/bash; pkill kodi; pkill pcmanfm; retroarch' > retroarch.sh
-
-#touch pcmanfm.sh
-#echo '!#/bin/bash; pcmanfm' > pcmanfm.sh
-
-#touch kdeconnect.sh
-#echo '!#/bin/bash; kcmshell5 kcm_kdeconnect' > kdeconnect.sh
-
-#touch runner.sh
-#echo '!#/bin/bash; pkill retroarch; pkill pcmanfm; pkill kodi; lxpanelctl run' > runner.sh
-
-# Le damos permiso a los archivos
-#sudo chmod 777 ./kodi.sh ./retroarch.sh ./pcmanfm.sh ./kdeconnect.sh ./runner.sh
-
-
-# Surgerencias de atajos del teclado
-
-# Kodi > ALT + K
-# Retroarch > ALT + R
-# Explorador de Archivos > ALT + A
-# Kde-Connet > ALT + C
-# Lanzador > ALT + B (Por defecto es ALT + F2)
-
-# Añadir atajos al teclado
-# manual
-# menu > Preferencias > Setup Hot Keys > Programs
 
 
 
